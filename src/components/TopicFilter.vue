c2ee9a05b787efb5da719b2857fd9f102e417aee
Ajiri Oshomah Iyelobu <oshomahiyelobu1@gmail.com>
2026-08-23 21:47:38 +0100

    feat: add HealthWaka topic filter

---
+++ src/components/TopicFilter.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  topics: {
+    type: Array,
+    default: () => [],
+  },
+  modelValue: {
+    type: String,
+    default: '',
+  },
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="topic-row" aria-label="Filter by topic">
+    <button
+      class="topic-chip"
+      :class="{ selected: modelValue === '' }"
+      type="button"
+      @click="$emit('update:modelValue', '')"
+    >
+      All topics
+    </button>
+
+    <button
+      v-for="topic in topics"
+      :key="topic.slug"
+      class="topic-chip"
+      :class="{ selected: modelValue === topic.slug }"
+      type="button"
+      @click="$emit('update:modelValue', topic.slug)"
+    >
+      {{ topic.name }}
+    </button>
+  </div>
+</template>

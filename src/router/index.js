5a7234cab1ad4e2eac01cde1e677e934d6cf1106
Ajiri Oshomah Iyelobu <oshomahiyelobu1@gmail.com>
2026-08-23 21:48:30 +0100

    feat: add HealthWaka routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ArticleDetailView from '../views/ArticleDetailView.vue'
+import AskQuestionView from '../views/AskQuestionView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/articles/:slug',
+      name: 'article-detail',
+      component: ArticleDetailView,
+    },
+    {
+      path: '/ask',
+      name: 'ask',
+      component: AskQuestionView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView,
+    },
+  ],
+  scrollBehavior() {
+    return { top: 0, behavior: 'smooth' }
+  },
 })
 
 export default router
